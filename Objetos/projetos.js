b1ac4969fc3fd55c852e6c8ba4c9a258baad7201
Pixaiti <20251AC0080013@estudantes.ifpr.edu.br>
2026-06-19 15:24:53 -0300

    olha o JS vindo

---
+++ Objetos/projetos.js
@@ -0,0 +1,76 @@
+class projeto {
+    constructor(idProjeto, tipo, estagio, dataPublicacao, dataAtualizacao, numeroVizualizacoes, numeroCurtidas, situacaoProjeto, titulo, descricao, capa){
+        this.idProjeto = idProjeto
+        this.tipo = tipo
+        this.estagio = estagio
+        this.dataPublicacao = dataPublicacao
+        this.dataAtualizacao = dataAtualizacao
+        this.numeroVizualizacoes = numeroVizualizacoes
+        this.numeroCurtidas = numeroCurtidas
+        this.situacaoProjeto = situacaoProjeto
+        this.titulo = titulo
+        this.descricao = descricao
+        this.capa = capa
+    }
+}
+
+const projetoTeste1 = new projeto(
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    "Wireframes/X.webp"
+)
+
+const projetoTeste2 = new projeto(
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    "Wireframes/X.webp"
+)
+
+const projetoTeste3 = new projeto(
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    "Wireframes/X.webp"
+)
+
+const projetosDestaque = document.getElementById("projetos-destaque")
+const listaProjetos = []
+let htmlAcumulado = ""
+
+listaProjetos.push(projetoTeste1)
+listaProjetos.push(projetoTeste2)
+listaProjetos.push(projetoTeste3)
+
+console.log(listaProjetos)
+console.log("oi")
+
+listaProjetos.forEach(projeto =>{
+    htmlAcumulado += `
+     <div class="destaque${projeto.idProjeto}" id="${projeto.id}"><img src="${projeto.capa}" alt=""></div>
+    `
+})
+
+projetosDestaque.innerHTML = htmlAcumulado
